9f796b5277649061114471d5c11e572be771ec10
mzqan <123510256+mzqan@users.noreply.github.com>
2026-09-17 15:07:03 -0400

    v7 soln: - cleaner, templated view

--- src/submission.hpp
+++ src/submission.hpp
@@ -8,113 +8,110 @@
 // boundary vs interior v4: {"runtime_ms": 223.805, "memory_mb": 16.777, "score": 1.003}
 // views & simd v5: {"runtime_ms": 213.517, "memory_mb": 16.777, "score": 1.032}
 // open mp v6: {"runtime_ms": 116.867, "memory_mb": 16.777, "score": 2.174}
+// templated view v7: {"runtime_ms": 119.108, "memory_mb": 16.777, "score": 2.391}
 
-struct GridView {
-  double* data;
-  std::size_t rows;
-  std::size_t cols;
-  std::size_t stride;
-  
-  double& operator()(std::size_t i, std::size_t j) const{
-    return data[i * stride + j];
-  }
+template <typename T>
+struct View {
+    T* data;
+
+    std::size_t rows;
+    std::size_t cols;
+    std::size_t stride;
+
+    T& operator()(std::size_t i, std::size_t j) const {
+        return data[i * stride + j];
+    }
 };
 
-struct ReadOnlyGridView {
-  const double* data;
-  std::size_t rows;
-  std::size_t cols;
-  std::size_t stride;
-  
-  double operator()(std::size_t i, std::size_t j) const{
-    return data[i * stride + j];
-  }
-};
 
 class Grid {
 private:
-  std::size_t rows_;
-  std::size_t cols_;
-  std::size_t stride_;
-  std::vector<double> data_;
+    std::size_t rows_;
+    std::size_t cols_;
+    std::size_t stride_;
+    std::vector<double> data_;
 
 public:
-  Grid(std::size_t rows, std::size_t cols)
-    : rows_(rows)
-    , cols_(cols)
-    , stride_(cols)
-    , data_(rows * stride_) {}
+    Grid(std::size_t rows, std::size_t cols)
+        : rows_(rows)
+        , cols_(cols)
+        , stride_(cols)
+        , data_(rows * stride_) {}
 
   // returns reference to cell, allows r/w
-  double& operator()(std::size_t i, std::size_t j){
-    return data_[i * stride_ + j];
-  }
+    double& operator()(std::size_t i, std::size_t j){
+        return data_[i * stride_ + j];
+    }
 
   // returns copy to cell, read-only
-  double operator()(std::size_t i, std::size_t j) const{
-    return data_[i * stride_ + j];
-  }
+    double operator()(std::size_t i, std::size_t j) const{
+        return data_[i * stride_ + j];
+    }
 
-  std::size_t rows() const{
-    return rows_;
-  }
+    std::size_t rows() const{
+        return rows_;
+    }
 
-  std::size_t cols() const{
-    return cols_;
-  }
+    std::size_t cols() const{
+        return cols_;
+    }
 
-  std::size_t stride() const{
-    return stride_;
-  }
+    std::size_t stride() const{
+        return stride_;
+    }
 
-  GridView view() {
-    return {data_.data(), rows_, cols_, stride_};
-  }
+    // Mutable Grid -> mutable View
+    View<double> view(){
+        return {data_.data(), rows_, cols_, stride_};
+    }
 
-  ReadOnlyGridView view() const{
-    return {data_.data(), rows_, cols_, stride_};
-  }
-};  
+    // Const Grid -> read-only View
+    View<const double> view() const {
+        return {data_.data(), rows_, cols_, stride_};
+    }
+};
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid
 void apply_stencil(const Grid& old_grid, Grid& new_grid){
-  ReadOnlyGridView old_view {old_grid.view()};
-  GridView new_view {new_grid.view()};
+    auto old_view {old_grid.view()};
+    auto new_view {new_grid.view()};
 
-  const std::size_t rows {old_grid.rows()};
-  const std::size_t cols {old_grid.cols()};
+    const std::size_t rows {old_grid.rows()};
+    const std::size_t cols {old_grid.cols()};
 
-  // top-bottom boundary unchanged
-  for (std::size_t i = 0; i < cols; ++i){
-      new_view(0, i) = old_view(0, i);
-      new_view(rows - 1, i) = old_view(rows - 1, i);
-  }
+    // top-bottom boundary unchanged
+    for (std::size_t i = 0; i < cols; ++i){
+        new_view(0, i) = old_view(0, i);
+        new_view(rows - 1, i) = old_view(rows - 1, i);
+    }
 
-  //left-right boundary unchanged
-  for (std::size_t i = 1; i < rows - 1; ++i){
-      new_view(i, 0) = old_view(i, 0);
-      new_view(i, cols - 1) = old_view(i, cols - 1);
-  }
+    //left-right boundary unchanged
+    for (std::size_t i = 1; i < rows - 1; ++i){
+        new_view(i, 0) = old_view(i, 0);
+        new_view(i, cols - 1) = old_view(i, cols - 1);
+    }
 
-  // parallelize rows (outputs independent from one another)
-  #pragma omp parallel for
-  for (std::size_t i = 1; i < rows - 1; ++i){
-      const std::size_t row_start{i * old_view.stride};
+    // parallelize rows (outputs independent from one another)
+    #pragma omp parallel for
+    for (std::size_t i = 1; i < rows - 1; ++i){
 
-      // vectorize inner loop (cols are contiguous in memory)
-      #pragma omp simd
-      for (std::size_t j = 1; j < cols - 1; ++j){
-          const std::size_t index{row_start + j};
+        const std::size_t row_start {i * old_view.stride};
 
-          new_view.data[index] =
-              0.5 * old_view.data[index]
-              + 0.125 * (
-                  old_view.data[index - old_view.stride]
-                  + old_view.data[index + old_view.stride]
-                  + old_view.data[index - 1]
-                  + old_view.data[index + 1]
-              );
-      }
-  }
+        // vectorize inner loop (cols are contiguous in memory)
+        #pragma omp simd
+        for (std::size_t j = 1; j < cols - 1; ++j){
+
+            const std::size_t index {row_start + j};
+
+            new_view.data[index] =
+                0.5 * old_view.data[index]
+                + 0.125 * (
+                    old_view.data[index - old_view.stride]
+                    + old_view.data[index + old_view.stride]
+                    + old_view.data[index - 1]
+                    + old_view.data[index + 1]
+                );
+        }
+    }
 }
